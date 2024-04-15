a9c2c834f0f77c051dff177a4f393e23b5267d78
HP <HP@DESKTOP-4E0PRTP>
2024-04-15 23:34:13 +0200

    Add project files.

---
+++ WebApplication20/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication20.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:Label ID="Label5" runat="server" Text="ime naloga"></asp:Label>
+    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+    <asp:Label ID="Label6" runat="server" Text="lozinka"></asp:Label>
+    <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+    <asp:Button ID="Button2" runat="server" Text="prijava" OnClick="Button2_Click" />
+
+    <asp:Label ID="Label1" runat="server" Text="prijava radnika" Visible="false"></asp:Label>
+
+    <br/><br/>
+
+    <asp:Label ID="Label2" runat="server" Text="ime" Visible="false"></asp:Label>
+
+    <asp:TextBox ID="TextBox1" runat="server" Visible="false"></asp:TextBox>
+
+    <asp:Label ID="Label3" runat="server" Text="prezime" Visible="false"></asp:Label>
+
+    <asp:TextBox ID="TextBox2" runat="server" Visible="false"></asp:TextBox>
+
+    <asp:Label ID="Label4" runat="server" Text="datum rodjenja" Visible="false"></asp:Label>
+
+    <asp:TextBox ID="TextBox3" runat="server" Visible="false"></asp:TextBox>
+
+    <asp:Button ID="Button1" runat="server" Text="upisi" Visible="false" OnClick="Button1_Click"/>
+
+    <asp:GridView ID="GridView1" runat="server" CssClass="table" AutoGenerateColumns="true"></asp:GridView>
+
+
+
+</asp:Content>
---
+++ WebApplication20/WebForm1.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Drawing;
+
+namespace WebApplication20
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=gradiliste;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (con)
+                {
+                    con.Open();
+
+                    GridViewFill(con);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception Message: " + ex.Message);
+                System.Diagnostics.Debug.WriteLine("Stack Trace: " + ex.StackTrace);
+            }
+        }
+        void GridViewFill(SqlConnection con)
+        {
+            string query = "SELECT * FROM Radnik";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            GridView1.DataSource = reader;
+
+            GridView1.DataBind();
+
+            reader.Close();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (TextBox1.Text == "" && TextBox2.Text == "" && TextBox3.Text == "")
+            {
+                TextBox1.BorderColor = Color.Red;
+                TextBox2.BorderColor = Color.Red;
+                TextBox3.BorderColor = Color.Red;
+            }
+            else
+            {
+                using (con)
+                {
+                    con.Open();
+                    string searchParameter = TextBox1.Text;
+                    string searchParameter2 = TextBox1.Text;
+                    string searchParameter3 = TextBox3.Text;
+                    SqlParameter p = new SqlParameter();
+                    SqlParameter p2 = new SqlParameter();
+                    SqlParameter p3 = new SqlParameter();
+                    p.Value = searchParameter;
+                    p2.Value = searchParameter2;
+                    p3.Value = searchParameter3;
+                    p.ParameterName = "@searchParameter";
+                    p2.ParameterName = "@searchParameter2";
+                    p3.ParameterName = "@searchParameter3";
+                    string query = "Update Radnik " +
+                                   "Set Ime=@searchParameter,Prezime=@searchParameter1,DatumRodjenja=@searchParameter2  " +
+                                   "Where RadnikID = 1 ";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.Add(p);
+                    cmd.Parameters.Add(p2);
+                    cmd.ExecuteNonQuery();
+                    TextBox1.Text = "";
+                    TextBox2.Text = "";
+                    TextBox3.Text = "";
+
+
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (TextBox4.Text == "Sekula" && TextBox5.Text == "mrzimskolu")
+            {
+                Label1.Visible=true;
+                Label2.Visible=true;
+                TextBox1.Visible=true;
+                Label3.Visible=true;
+                TextBox2.Visible=true;
+                Label4.Visible=true;
+                TextBox3.Visible=true;
+                Button1.Visible=true;
+            }
+        }
+    }
+}
